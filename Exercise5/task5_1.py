1d31c12f069fcb9b66672c71acbed6fbbf78b36c
Tatiana <laviento@gmail.com>
2018-03-09 23:13:18 +0400

    2 задания на 5 урок

--- Exercise5/task5_1.py
+++ Exercise5/task5_1.py
@@ -6,11 +6,18 @@ try:
     if len(str(x))!=3:
         print('Вы ввели не трёхзначное число')
     else:
-        n, p = 1, 1
-        for i in range(2, -1, -1):
-            n = x // (10 ** i)
-            x %= (10 ** i)
-            p = p * n
-        print('Произведение равно {:d}'.format(p)) 
+        p = 1
+        s = str(x)
+        
+        #Вариант 1
+        for i in s:            
+            p = p * int(i)
+        print('1. Произведение равно {:d}'.format(p)) 
+
+        #Вариант 2
+        p = 1
+        for i in range(len(s)):
+            p = p * int(s[i])
+        print('2. Произведение равно {:d}'.format(p)) 
 except:
     print("Некорректные данные")
